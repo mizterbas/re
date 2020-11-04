d2a4941b8a0c191893bfc0a315459ade710358df
mizterbas <48507876+mizterbas@users.noreply.github.com>
2020-11-04 20:39:28 +1100

    Update classifier_utils.py

--- classifier_utils.py
+++ classifier_utils.py
@@ -261,7 +261,45 @@ class MedNLIProcessor(DataProcessor):
             examples.append(
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
-  
+      
+class ChemProtProcessor(DataProcessor):
+
+    def get_train_examples(self, data_dir):
+        l1 = self._read_tsv(os.path.join(data_dir, "train.tsv"))
+        return self._create_examples(l1, "train")
+
+    def get_dev_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        """See base class."""
+        return [i.lower() for i in ["CPR:3", "CPR:4", "CPR:5", "CPR:6", "CPR:9", "false"]]
+    
+    def _create_examples(self, lines, set_type):
+       """Creates examples for the training and dev sets."""
+       examples = []
+       for (i, line) in enumerate(lines):
+           #skip header
+           if i==0 and set_type=='test':
+               continue
+           guid = line[0]
+           text_a = self.process_text(line[1])
+           if set_type == "test":
+               label = self.get_labels()[-1]
+           else:
+               try:
+                   label = self.process_text(line[2])
+               except IndexError:
+                   logging.exception(line)
+                   exit(1)
+           examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+       return examples
+    
 class ColaProcessor(DataProcessor):
   """Processor for the CoLA data set (GLUE version)."""
 
